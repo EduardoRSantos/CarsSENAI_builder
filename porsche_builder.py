ab771be443c02a1834ffd2d14ecae4cd60967b51
EduardoRSantos <eduardoow6@gmail.com>
2023-02-17 23:24:02 -0300

    1.0

---
+++ porsche_builder.py
@@ -0,0 +1,24 @@
+from body import Body
+from builder import Builder
+from engine import Engine
+from wheel import Wheel
+
+
+class PorscheBuilder(Builder):
+    def __init__(self):
+        self.wheel = Wheel()
+        self.engine = Engine()
+        self.body = Body()
+
+        self.wheel.size = 20
+        self.engine.horsepower = None
+        self.body.shape = None
+
+    def get_wheel(self) -> Wheel:
+        return self.wheel
+
+    def get_engine(self) -> Engine: 
+        return self.engine
+
+    def get_body(self) -> Body: 
+        return self.body
